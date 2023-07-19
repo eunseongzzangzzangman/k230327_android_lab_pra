10572b2446d1ce07a758a4883c1e0cbaaea7b1c8
정은성 <8ohowb@naver.com>
2023-07-19 16:20:09 +0900

    230719 7교시 실습 안내, 추가사항, 인트로 화면 , 토스트, 로그캣 이용방법.

--- app/src/main/java/com/example/k230327_android_lab_pra/JoinActivity.kt
+++ app/src/main/java/com/example/k230327_android_lab_pra/JoinActivity.kt
@@ -3,9 +3,9 @@ package com.example.k230327_android_lab_pra
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity2 : AppCompatActivity() {
+class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login2)
+        setContentView(R.layout.activity_join)
     }
 }
