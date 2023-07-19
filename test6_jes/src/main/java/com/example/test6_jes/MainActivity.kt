7e8b2859e6b9d8b0b8e628dd11345a9ffb51f776
정은성 <8ohowb@naver.com>
2023-07-19 13:56:39 +0900

    230719 4교시 6 뷰 및 바인딩, 기본 이벤트 처리

--- test6_jes/src/main/java/com/example/test6_jes/MainActivity.kt
+++ test6_jes/src/main/java/com/example/test6_jes/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.test6_jes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.test6_jes.R
 
+//액티비티 : 화면을 그려주는 역할
 class MainActivity : AppCompatActivity() {
+    //최초 한번 화면에 출력을 하는 역할
     override fun onCreate(savedInstanceState: Bundle?) {
+        // savedInstanceState: Bundle? -> 임시 데이터 저장 객체
         super.onCreate(savedInstanceState)
+        // 해당 레이아웃이 샘플로 리니어 사용하는 중
+        // 이유? 제약 조건 설정이 간단해서
+        // 뷰만 선택해서 배치만 하면 기본 세로, 가로방향으로 배치가 간단함
         setContentView(R.layout.activity_main)
     }
 }
